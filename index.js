2a098aea5814d82f83e78a1f6c152a33e2079fc0
Tom Rohr <Tom.Rohr@intellitect.com>
2023-08-01 10:33:08 -0700

    Proj setup, player stand-in, gravity

---
+++ index.js
@@ -0,0 +1,46 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const gravity = 0.7;
+
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 30;
+    this.height = 30;
+  }
+
+  draw() {
+    context.fillStyle = "red";
+    context.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+const player = new Player();
+
+function animate() {
+  requestAnimationFrame(animate);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+animate();
